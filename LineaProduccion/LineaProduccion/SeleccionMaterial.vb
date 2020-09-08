4fd76d677c3d5499f2ebe102be5fad9542e3ea67
julio123-0 <julyiocesar84@gmail.com>
2020-09-08 14:32:32 -0500

    mensaje

--- LineaProduccion/LineaProduccion/SeleccionMaterial.vb
+++ LineaProduccion/LineaProduccion/SeleccionMaterial.vb
@@ -1,3 +1,15 @@
 ﻿Public Class SeleccionMaterial
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        SeleccionTenis.Show()
 
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Hide()
+        SeleccionColores.Show()
+
+
+    End Sub
 End Class
